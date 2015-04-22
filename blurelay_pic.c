f39a6200c73c7fca596c21be11050c89f92e6c77
samirfor <samirfor@gmail.com>
2015-04-22 20:52:53 -0300

    Adicionado link do youtube e artigo

--- blurelay_pic.c
+++ blurelay_pic.c
@@ -1,7 +1,9 @@
 /**
  * Ferramenta SanUSB: http://sanusb.site50.net/
  * Compilar com MPLABX+C18: https://drive.google.com/open?id=0B5332OAhnMe2N3czQWxVX0JVSkE&authuser=0
- * Apk BluRelay para Android: https://github.com/samirfor/BluRelay/blob/master/BluSwitch.apk?raw=true
+ * Apk BluRelay para Android: https://github.com/samirfor/BluRelay/blob/master/BluRelay.apk?raw=true
+ * Vídeo: https://www.youtube.com/watch?v=X2ADGWf2mXo
+ * Artigo: https://www.dropbox.com/s/3vuy43qf84ef089/Interpola%C3%A7%C3%A3o%20para%20sinaliza%C3%A7%C3%A3o%20ou%20chaveamento%20de%20cargas%20el%C3%A9tricas%20remotamente%20via%20Bluetooth%20e%20localmente%20via%20potenci%C3%B4metro.pdf?dl=0
  *
  * Este programa recebe comandos do app BluRelay via bluetooth ou via potenciômetro.
  *
